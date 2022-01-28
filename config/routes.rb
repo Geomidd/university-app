986a3b8c2c5a1776ec6fcaa10dd55c8fb1510740
George Middleton <georgemiddleton3@gmail.com>
2022-01-27 20:30:41 -0700

    Add enrollment feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "login", to: "logins#new"
   post "login", to: "logins#create"
   delete "logout", to: "logins#destroy"
+  post "course_enroll", to: "enrollments#create"
   resources :courses, only: [:show, :new]
   resources :students, except: [:destroy]
 end
